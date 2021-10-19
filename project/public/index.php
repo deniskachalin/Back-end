edd8f862e04181fcc6285cc089181d624fa43ff4
Denis Kachalin <deniskachalin@yandex.ru>
2021-10-19 12:54:18 +0800

    Лаба 4 закончена

--- project/public/index.php
+++ project/public/index.php
@@ -11,13 +11,33 @@ $twig = new \Twig\Environment($loader);
 
 $url = $_SERVER["REQUEST_URI"];
 
+$title = "";
+$template = "";
+$is_image = false;
+$is_info = false;
+
 if ($url == "/") {
-    echo $twig->render("main.php");
+    $title = "Главная";
+    $template = "main.twig";
 } elseif (preg_match("#/doom-2016#", $url)) {
-    echo $twig->render("doom.php");
+    $title = "Doom 2016";
+    $template = "doom.twig";
+    $context['is_image'] = $url == "/doom-2016/image";
+    $context['image'] = "/images/doom-2016.jpg";
+    $context['is_info'] = $url == "/doom-2016/info";
 } elseif (preg_match("#/persona-2#", $url)) {
-    echo $twig->render("persona2.php");
+    $title = "Persona 2";
+    $template = "persona2.twig";
+    $context['is_image'] = $url == "/persona-2/image";
+    $context['image'] = "/images/persona-2.png";
+    $context['is_info'] = $url == "/persona-2/info";
 } 
+
+
+
+$context['title'] = $title;
+
+echo $twig->render($template, $context);
        
    
 
