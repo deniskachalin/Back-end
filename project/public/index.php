3885cc2d5717d6a9f26d8ae8d3750bbd98c0ddb2
Denis Kachalin <deniskachalin@yandex.ru>
2021-10-24 17:22:35 +0800

    5 лаба закончена

--- project/public/index.php
+++ project/public/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once '../vendor/autoload.php';
 
 // создаем загрузчик шаблонов, и указываем папку с шаблонами
@@ -17,7 +17,7 @@ $is_image = false;
 $is_info = false;
 
 $context = [];
-$menu = [
+$nav = [
     [
         "title" => "Главная",
         "url" => "/",
@@ -32,30 +32,58 @@ $menu = [
     ],
 ];
 
+$menu = [
+    [
+        "title" => "Doom 2016",
+        "url" => "/doom-2016",
+        "img_url" => "/doom-2016/image",
+        "info_url" => "/doom-2016/image",
+    ],
+    [
+        "title" => "Persona 2",
+        "url" => "/persona-2",
+        "img_url" => "/persona-2/image",
+        "info_url" => "/persona-2/image",
+    ],
+];
+
 if ($url == "/") {
     $title = "Главная";
     $template = "main.twig";
 } elseif (preg_match("#/doom-2016#", $url)) {
     $title = "Doom 2016";
-    $template = "doom.twig";
-    $context['is_image'] = $url == "/doom-2016/image";
-    $context['image'] = "/images/doom-2016.jpg";
-    $context['is_info'] = $url == "/doom-2016/info";
+    $template = "__object.twig";
+    $context['image_url'] = "/doom-2016/image";  
+    $context['info_url'] = "/doom-2016/info";  
+    if (preg_match("#^/doom-2016/image#", $url)){
+        $template = "image.twig";
+        $context['is_image'] = $url == "/doom-2016/image";
+         $context['image'] = "/images/doom-2016.jpg";
+    }elseif (preg_match("#^/doom-2016/info#", $url)){
+        $template = "doom.twig";
+        $context['is_info'] = $url == "/doom-2016/info";
+    }
 } elseif (preg_match("#/persona-2#", $url)) {
     $title = "Persona 2";
-    $template = "persona2.twig";
-    $context['is_image'] = $url == "/persona-2/image";
-    $context['image'] = "/images/persona-2.png";
-    $context['is_info'] = $url == "/persona-2/info";
-} 
+    $template = "__object.twig";
+    $context['image_url'] = "/persona-2/image";  
+    $context['info_url'] = "/persona-2/info";  
+    if (preg_match("#^/persona-2/image#", $url)){
+        $template = "image.twig";
+        $context['is_image'] = $url == "/persona-2/image";
+        $context['image'] = "/images/persona-2.jpg";
+    }elseif (preg_match("#^/persona-2/info#", $url)){
+        $template = "persona2.twig";
+        $context['is_info'] = $url == "/persona-2/info";
+    }
+    
+    
+}
 
 
 
 $context['title'] = $title;
+$context['nav'] = $nav;
 $context['menu'] = $menu;
 
 echo $twig->render($template, $context);
-       
-   
-
-    
